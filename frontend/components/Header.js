e7f57e170b56e992da7ef915ad153b524be337da
Tania Germanlieva <tania.germanlieva@gmail.com>
2023-01-15 22:42:52 +0100

    add pages

---
+++ frontend/components/Header.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import Nav from './Nav.js';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  background: red;
+  a {
+    color: white;
+  }
+`;
+
+export default function Header() {
+  return (
+    <header>
+      <div className="bar">
+        <Logo>
+          <Link href="/">Sick fits</Link>
+        </Logo>
+      </div>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
+      <Nav />
+    </header>
+  );
+}
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,7 +1,3 @@
 export default function SellPage() {
-  return (
-    <div>
-      <p>Hello!</p>
-    </div>
-  );
+  return <p>Hello from the sell page!</p>;
 }
