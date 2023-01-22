1c27d5a865509b9045b75e1dd70c83fcd1ee345b
Tania Germanlieva <tania.germanlieva@gmail.com>
2023-01-22 22:21:03 +0100

    add global styles

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -6,15 +6,35 @@ const Logo = styled.h1`
   font-size: 4rem;
   margin-left: 2rem;
   position: relative;
+  z-index: 2;
   background: red;
+  transform: skew(-7deg);
   a {
     color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
   }
 `;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className='bar'>
         <Logo>
           <Link href='/'>Sick fits</Link>
@@ -24,6 +44,6 @@ export default function Header() {
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
